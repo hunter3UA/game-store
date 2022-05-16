6de038009b400c8c3c8ed5e2a7d0895583252cf2
Oleksandr Solontsov <solontsov04s@gmail.com>
2022-05-16 10:14:37 +0300

    Fix publisherDTO

--- GameStore.BLL/DTO/Publisher/PublisherDTO.cs
+++ GameStore.BLL/DTO/Publisher/PublisherDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using GameStore.BLL.DTO.Game;
 
 namespace GameStore.BLL.DTO.Publisher
@@ -13,6 +14,7 @@ namespace GameStore.BLL.DTO.Publisher
 
         public string HomePage { get; set; }
 
+        [JsonIgnore]
         public List<GameDTO> Games { get; set; }
     }
 }
